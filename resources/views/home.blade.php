16bded49e593a2b128811a039d8241c7acb2da97
palrei <palrei@gmail.com>
2023-11-22 11:00:17 +0700

    awal

---
+++ resources/views/home.blade.php
@@ -0,0 +1,48 @@
+@extends("tempalate")
+
+@section("title")
+Home
+@endsection
+
+@section("title")
+<h1>
+    Tempat Wisata
+</h1>
+<h3>
+    Banda Neira
+</h3>
+<img src="{{ url('/assets/bandaneira.jpg') }}" class="img-fluid">
+<p>
+Banda Tangah atau Banda Naira adalah salah satu pulau di Kepulauan Banda, 
+dan merupakan pusat administratif Kecamatan Banda, Kabupaten Maluku Tengah, Maluku, Indonesia.
+</p>
+<br>
+
+<h3>
+   Kerajaan Mataram Kuno 
+</h3>
+<img src="{{ url('/assets/mataramkuno.jpg') }}" class="img-fluid">
+<p>
+Kerajaan Mataram Kuno memiliki dua periode berdasarkan lokasi atau ibu kota pemerintahannya. 
+Pertama adalah periode awal Kerajaan Medang yaitu di Jawa Tengah di bawah Wangsa Sanjaya dan 
+Sailendra (732-929 M), serta yang kedua ketika pindah ke Jawa Timur dan dikuasai oleh 
+Wangsa Isyana (929-1016 M). Pada 929 M, Kerajaan Mataram Kuno dipindahkan ke Jawa Timur oleh Mpu Sindok. 
+Menurut George Coedes dalam The Indianized states of Southeast Asia (1968).
+</p>
+<br>
+
+<h3>
+    Raja Ampat
+</h3>
+<img src="{{ url('/assets/rajaampat.jpg') }}" class="img-fluid">
+<p>
+Kepulauan Raja Ampat adalah gugusan kepulauan yang berlokasi di 
+barat bagian Semenanjung Kepala Burung (Vogelkoop) Pulau Papua.
+</p>
+<br>
+
+<br>
+<p>
+    Haloo Kunyuk
+</p>
+@endsection
