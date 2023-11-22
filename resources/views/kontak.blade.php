a8ffc40799a8c83b6ac840a24ab1670b75465aa3
saepudin765 <ssaepudin500@gmail.com>
2023-11-22 11:27:13 +0700

    hum

---
+++ resources/views/kontak.blade.php
@@ -0,0 +1,11 @@
+@extends("template")
+
+@section("title")
+kontak
+@endsection
+
+@section("content")
+<p>
+    Halo, ini adalah halaman kontak
+</p>
+@endsection
---
+++ resources/views/profil.blade.php
@@ -0,0 +1,11 @@
+@extends("template")
+
+@section("title")
+profile
+@endsection
+
+@section("content")
+<p>
+    Halo, ini adalah halaman profile
+</p>
+@endsection
