70031720a7827d3a496ad3c13a6fc539588ae55e
Riko Rizky <rikorizky20@gmail.com>
2023-11-23 20:03:47 +0700

    fssaas

--- resources/views/tambah_wisata.blade.php
+++ resources/views/tambah_wisata.blade.php
@@ -1,3 +1,60 @@
-<div>
-    <!-- Happiness is not something readymade. It comes from your own actions. - Dalai Lama -->
-</div>
+@extends("template")
+
+@section("title")
+Tambah wisata
+@endsection
+
+@section("content")
+<form method="post" action="{{ url('tambah-wisata') }}">
+    @csrf
+    <div class="mb-2">
+        <label class="form-label">ID</label>
+        <input type="text" name="ID" class="form-control" required>
+    </div>
+    <div class="mb-2">
+        <label class="form-label">Name</label>
+        <input type="text" name="name" class="form-control" required>
+    </div>
+    <div class="form-check">
+        <label>
+                <input type="radio" class="form-check-input" name="jk" value="P">
+                Prempuan
+            </label>
+        </div>
+    <div class="mb-2">
+        <label class="form-label">Tempat Lahir</label>
+            <input type="text" name="tempat_lahir" class="form-control" required>
+    </div>
+    <div class="mb-2">
+        <label class="form-label">Tanggal Lahir</label>
+            <input type="date" name="tgl_lahir" class="form-control" required>
+    </div>
+    
+    <div class="mb-2">
+        <label class="form-label">No. Hp</label>
+        <input type="text" name="hp" class="form-control" required>
+    </div>
+    <div class="mb-2">
+        <label class="form-label">Alamat</label>
+        <textarea class="form-control" name="alamat" required></textarea>
+    </div>
+    <div class="mb-2">
+        <label class="form-label">Jenis Kelamin
+            </label>
+        </div>
+        <div class="form-label">
+            <label>
+            <input type="radio" class="form-check-input" name="jk" value="L" checked>
+            Laki-Laki
+        </label>
+    </div>
+    <div class="mb-5">
+        <button type="submit" class="btn btn-primary">
+            Tambah
+        </button>
+        <a href="{{url('data-pegawai')}}" class="btn btn-danger">
+            Kembali
+        </a>
+    </div>
+</form>
+@endsection
